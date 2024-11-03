4ec663ed3ef34be2b4dbbe4c745c87214954cc9d
Mageseeni <mageseeni4@gmail.com>
2024-11-04 01:30:34 +0530

    Add files via upload

---
+++ Practice.1.py
@@ -0,0 +1,49 @@
+# writing my first python code
+#print("Hello world!")
+
+#print("my python learners")
+
+#name = "mage"
+#print(name)
+#name="hope"
+#print(name)
+
+#strings taxt and values and numbers
+#mynumber = "358"
+#print(mynumber)
+#print(type(mynumber))
+
+#Length of String
+
+#mylength="my name mageshwari"
+#print(mylength)
+#print(len(mylength))
+
+#Slicing Strings
+
+#myvalue= "i love you"
+#print(myvalue[3:5])  #3,4
+#print(len(myvalue)) #lengtth 
+#print(myvalue[2:8]) #2,3,4,5,6,7
+#print(myvalue[:]) # full value
+#print(myvalue[2:]) #till End
+#print(myvalue[:5])
+#print(myvalue[-5:-2])
+#print(myvalue[-8:-2])
+#print(myvalue[0:10:2])
+#print(myvalue[::4])
+#print(myvalue[::-1]) # reversing a string
+
+
+#Palindrome Program
+
+user_input = "madam"
+
+reversed_string =user_input[::-1]
+print(reversed_string)
+
+if user_input == reversed_string: # "madam" == "madam"
+    print("Palindrome") # Indentation through single tab, or use four space bars
+else:
+    print("Not Palindrome")
+
